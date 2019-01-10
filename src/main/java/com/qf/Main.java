731bcbc331bdc54aadbcca735301016cb5228ba8
ken <ken@qq.com>
2019-01-10 15:50:12 +0800

    架构第一次提交

---
+++ src/main/java/com/qf/Main.java
@@ -0,0 +1,13 @@
+package com.qf;
+
+/**
+ * @Author ken
+ * @Date 2019/1/10
+ * @Version 1.0
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java!!!");
+    }
+}
